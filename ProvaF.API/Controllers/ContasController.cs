3f7d8c513c4974791812136c2524de277667cbae
Leandro Ribeiro <github@leandroribeiro.com>
2020-09-13 18:54:36 -0300

    Implementação da Rota de Saldo

---
+++ ProvaF.API/Controllers/ContasController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using ProvaF.Domain.Entities;
+using ProvaF.Domain.Exceptions;
+using ProvaF.Domain.Services;
+
+namespace ProvaF.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContasController : ControllerBase
+    {
+        private readonly IContaService _service;
+
+        public ContasController(IContaService service)
+        {
+            _service = service;
+        }
+        
+        [HttpGet("{numero}/saldo")]
+        [ProducesResponseType(typeof(decimal), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(BusinessRuleValidationException), (int)HttpStatusCode.InternalServerError)]
+        public ActionResult<decimal> Get([FromRoute] int numero)
+        {
+            var saldo = _service.ObterSaldo(numero);
+
+            return new OkObjectResult(saldo);
+        }
+    }
+}

bfaa6e1f992e5b95149ba002618ec661100d03a7
Leandro Ribeiro <github@leandroribeiro.com>
2020-09-14 10:11:56 -0300

    Adicionando seed de dados iniciais

--- ProvaF.Infrastructure/Data/ProvaFDbContext.cs
+++ ProvaF.Infrastructure/Data/ProvaFDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 using ProvaF.Domain.Entities;
 
 namespace ProvaF.Infrastructure.Data
@@ -17,6 +18,20 @@ namespace ProvaF.Infrastructure.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new ContaConfiguration());
+
+            SeedData(modelBuilder);
+        }
+
+        private static void SeedData(ModelBuilder modelBuilder)
+        {
+            // contas básicas para testes
+            var seed = new List<Conta> {new Conta(1), new Conta(2), new Conta(3)};
+
+            // atribuindo saldo
+            seed.ForEach(c => c.Depositar(c.Id * 100));
+
+            modelBuilder.Entity<Conta>()
+                .HasData(seed);
         }
     }
 }
---
+++ ProvaF.Infrastructure/Migrations/20200914125718_AdicionadoDados.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProvaF.Infrastructure.Migrations
+{
+    public partial class AdicionadoDados : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Contas",
+                columns: new[] { "Id", "Saldo" },
+                values: new object[,]
+                {
+                    { 1, 100 },
+                    { 2, 200 },
+                    { 3, 300 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+           
+        }
+    }
+}
